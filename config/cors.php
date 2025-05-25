196131a592740a6431f184dacccd119a6988f778
resultadiether <resultadiether@gmail.com>
2025-05-25 15:56:27 +0800

    konek

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
    
 
-    'paths' => ['api/*', 'login', 'register', 'logout', 'profile'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
@@ -15,5 +15,5 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ]; 
