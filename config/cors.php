92b4a4dd8de88f95217f8af6867fdb76c709644e
resultadiether <resultadiether@gmail.com>
2025-05-25 17:30:39 +0800

    env

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,7 @@ return [
 
     'allowed_origins' => [
         'https://front-end-nvxe.vercel.app',
+        'https://front-end-nvxe-la6zu7pgq-diether-resultas-projects.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
