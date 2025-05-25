25fe8a041470b5be17ed7272465c8403aa5a3d9f
resultadiether <resultadiether@gmail.com>
2025-05-25 09:21:17 +0800

    link

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,front-end-9kkl.vercel.app',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,front-end-nvxe-git-main-diether-resultas-projects.vercel.app',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
