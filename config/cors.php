f6e943852b4eecf45186347f15e82c08b5eb0f61
resultadiether <resultadiether@gmail.com>
2025-05-26 11:18:38 +0800

    Update cors.php gumana kana ikaw lng nmn problema

--- config/cors.php
+++ config/cors.php
@@ -18,15 +18,15 @@ return [
 
     'allowed_methods' => ['*'],
 
-   'allowed_origins' => [
-    'https://front-end-nvxe.vercel.app',
-    'https://front-end-nvxe-git-main-diether-resultas-projects.vercel.app',
-    'https://front-end-nvxe-g09zy4uq9-diether-resultas-projects.vercel.app',
-],
-
+    'allowed_origins' => [
+        'https://front-end-nvxe.vercel.app',
+        'https://front-end-nvxe-git-main-diether-resultas-projects.vercel.app',
+        'https://front-end-nvxe-g09zy4uq9-diether-resultas-projects.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [
         '^https:\/\/front-end-nvxe.*\.vercel\.app$',
+    ],
 
     'allowed_headers' => ['*'],
 
@@ -35,4 +35,5 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => true,
+
 ];
