b478e7e36b8813211e313337cdcc5e2a86c4c429
resultadiether <resultadiether@gmail.com>
2025-05-26 12:39:00 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,12 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    */
-
     'paths' => [
         'api/*',
         'sanctum/csrf-cookie',
@@ -21,12 +15,10 @@ return [
     'allowed_origins' => [
         'https://front-end-nvxe.vercel.app',
         'https://front-end-nvxe-git-main-diether-resultas-projects.vercel.app',
-        'https://front-end-nvxe-g09zy4uq9-diether-resultas-projects.vercel.app',
+        'https://front-end-nvxe-p2ynp6hmr-diether-resultas-projects.vercel.app',
     ],
 
-    'allowed_origins_patterns' => [
-        '^https:\/\/front-end-nvxe.*\.vercel\.app$',
-    ],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
